906c38c146b5c458316bebae76615bc5c05de77f
tomasz-paluch <tomasz.m.paluch@gmail.com>
2021-08-10 21:53:04 +0200

    Finish Making BudgetList an Iterable

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from . import Expense
 
 
@@ -10,6 +12,20 @@ class BudgetList:
         self.sum_overages = 0
         self.overages = []
     
+    def __len__(self):
+        return len(self.expenses) + len(self.overages)
+    
+    def __iter__(self):
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
+    
+    def __next__(self):
+        try:
+            return self.iter_e.__next__()
+        except StopIteration as stop:
+            return self.iter_o.__next__()
+    
     def append(self, item):
         if self.sum_expenses + item < self.budget:
             self.expenses.append(item)
@@ -18,8 +34,6 @@ class BudgetList:
             self.overages.append(item)
             self.sum_overages += item
 
-    def __len__(self):
-        return self.expenses.__len__ + self.overages.__len__
 
 def main():
     myBudgetList = BudgetList(1200)
@@ -31,5 +45,15 @@ def main():
 
     print('The count of all expenses: ' + str(len(myBudgetList)))
 
+    for entry in myBudgetList:
+        print(entry)
+
+    fig, ax = plt.subplots()
+    labels = ['Expenses', 'Overages', 'Budget']
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    ax.bar(labels, values, color = ['green', 'red', 'blue'])
+    ax.set_title('Your total expenses vs. total budget')
+    plt.show()
+
 if __name__ == "__main__":
-    main
+    main()
